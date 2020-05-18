7b3fe99910a3432cfa1bfdaa8da5432a67c145f0
Talita Roberti <talitaroberti@gmail.com>
2020-05-18 17:24:14 +0200

    Boostrapping app with create react app and skeleton of game.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Speed Typing Game</h1>
+      <textarea />
+      <h4>Time Remaining:</h4>
+      <button>Start!</button>
+      <h1>Word Count:</h1>
+    </div>
+  );
+}
+
+export default App;
