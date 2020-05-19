003d433798cbf64f27d5c3b3b0bd382e40a7312d
Talita Roberti <talitaroberti@gmail.com>
2020-05-19 12:34:37 +0200

    Initializing the timer when the player clicks the start button, instead of on reload.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [ words, setWords ] = useState('');
-  const [ countdownTimer, setCountdownTimer ] = useState(15);
+  const [ countdownTimer, setCountdownTimer ] = useState(5);
+  const [ gameStarted, setGameStarted ] = useState(false);
 
   useEffect(() => {
-    if(countdownTimer > 0) {
+    if(gameStarted && countdownTimer > 0) {
       setTimeout(() => {
         setCountdownTimer(time => time - 1);
       }, 1000);
+    } else if (countdownTimer === 0) {
+      setGameStarted(false);
     }
-  }, [ countdownTimer ]);
+  }, [ countdownTimer, gameStarted ]);
 
   const handleTextarea = event => {
     setWords(event.target.value);
@@ -23,12 +26,16 @@ const App = () => {
     return text.trim().split(' ').length;
   }
 
+  const handleStartGame = () => {
+    setGameStarted(true);
+  }
+
   return (
     <div>
       <h1>Speed Typing Game</h1>
       <textarea value={ words } onChange={ handleTextarea } />
       <h4>Time Remaining: { countdownTimer }</h4>
-      <button>Start!</button>
+      <button onClick={ handleStartGame }>Start!</button>
       <h1>Word Count: { wordCount(words) }</h1>
     </div>
   );
